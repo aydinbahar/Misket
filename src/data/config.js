65bda9de83035cbd54e76ca563ebff07dcc60db7
Aydin Bahar <aydinbahar@netsense.com.tr>
2026-05-18 00:13:37 +0300

    chore: update version to 1.8.2 and adjust exam date in config

--- src/data/config.js
+++ src/data/config.js
@@ -1,8 +1,7 @@
 // Misket uygulama sabitleri
 //
-// LGS sınav tarihi: 2026 yılı için resmi tarih MEB takvimine göre kesinleştirilince
-// güncellenmeli. Şu an için 2026 Haziran ilk pazar günü olarak alındı.
-export const LGS_EXAM_DATE = new Date('2026-06-07T09:30:00');
+// LGS 2026 sınav tarihi: 13 Haziran 2026, MEB resmi tarih.
+export const LGS_EXAM_DATE = new Date('2026-06-13T09:30:00');
 
 // Günlük varsayılan hedef (kelime sayısı)
 export const DEFAULT_DAILY_GOAL = 10;
